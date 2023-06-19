c0d521f465f0895070761b204694cb41e6ce640f
antalmiklos <35046449+antalmiklos@users.noreply.github.com>
2023-06-19 15:55:48 +0200

    cr3 parsing stuff

--- main.go
+++ main.go
@@ -98,3 +98,185 @@ func convert(src string, destFile string) error {
 	}
 	return nil
 }
+
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Box struct {
+	Size uint32
+	Type [4]byte
+}
+type BigBox struct {
+	Size int64
+	Type [4]byte
+}
+
+func ReadData(f io.ReadSeeker, size int) ([]byte, error) {
+	d := make([]byte, size)
+	err := binary.Read(f, binary.BigEndian, &d)
+	if err != nil {
+		return nil, err
+	}
+	return d, nil
+}
+
+type MDAT struct {
+	Size      uint64
+	Type      [4]byte
+	LargeSize uint64
+	UUID      [16]byte
+}
+
+type DataBox struct {
+	Data []byte
+	Type string
+}
+
+type FTYP struct {
+	Box
+	Major [4]byte
+	Minor [4]byte
+}
+
+const ()
+
+func (b *FTYP) print() {
+	fmt.Println(string(b.Major[:]))
+	fmt.Println(string(b.Minor[:]))
+}
+
+func cmpType(t string, a [4]byte) bool {
+	for i, c := range a {
+		if c != t[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func writeToFile(d []byte, filename string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, _ = f.Write(d)
+}
+
+func findBox(container []byte) []DataBox {
+	var box Box
+	buf := bytes.NewReader(container)
+	boxes := []DataBox{}
+
+	for {
+		err := binary.Read(buf, binary.BigEndian, &box)
+		d := make([]byte, box.Size-8)
+		if err != nil {
+			break
+		}
+		err = binary.Read(buf, binary.BigEndian, &d)
+		if err != nil {
+			fmt.Println(err)
+		}
+		boxes = append(boxes, DataBox{Data: d, Type: string(box.Type[:])})
+		// buf.Seek(int64(box.Size)-8, io.SeekCurrent)
+	}
+	return boxes
+}
+
+func findTagIndex(box []byte, flag string) int {
+	return bytes.Index(box, []byte(flag))
+}
+
+func cr3() {
+	var acnt int
+	for {
+		acnt++
+		file, err := os.Open("test.cr3")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+		s, _ := file.Stat()
+		size := s.Size()
+
+		// var offset int64
+
+		var box Box
+		var offset int64
+		var moov []byte
+		var mdat []byte
+		for {
+			err = binary.Read(file, binary.BigEndian, &box)
+			if err != nil {
+				if err.Error() == "EOF" {
+					fmt.Println("end of file")
+					return
+				}
+				log.Fatal(err)
+			}
+			if cmpType("mdat", box.Type) {
+				file.Seek(offset, io.SeekStart)
+				mdat, _ = ReadData(file, int(size-offset))
+				break
+			} else if cmpType("moov", box.Type) {
+				moov, _ = ReadData(file, int(box.Size))
+				offset, _ = file.Seek(-8, io.SeekCurrent)
+			} else {
+				offset, _ = file.Seek(int64(box.Size)-8, io.SeekCurrent)
+			}
+		}
+		moovsub := findBox(moov)
+		var trakCnt int = 1
+		for _, v := range moovsub {
+			var jpgoffset uint32
+			var jpgsize uint32
+			if v.Type == "trak" {
+				traksub := findBox(v.Data)
+				for _, x := range traksub {
+					if x.Type == "mdia" {
+						mdiasub := findBox(x.Data)
+						for _, y := range mdiasub {
+							if y.Type == "minf" {
+								minfsub := findBox(y.Data)
+								for _, z := range minfsub {
+									if z.Type == "stbl" {
+										stblsub := findBox(z.Data)
+										for _, zz := range stblsub {
+											if zz.Type == "co64" {
+												jpgoffset = binary.BigEndian.Uint32(zz.Data[len(zz.Data)-4:])
+											}
+											if zz.Type == "stsz" {
+												jpgsize = binary.BigEndian.Uint32(zz.Data[len(zz.Data)-4:])
+											}
+											if jpgsize != 0 && trakCnt == 1 {
+												img := make([]byte, jpgsize)
+												jpgoffset = 16
+												img = mdat[jpgoffset : jpgsize+uint32(offset)]
+												writeToFile(img, "as2.jpg")
+											}
+										}
+
+									}
+								}
+							}
+						}
+					}
+				}
+				v.Type = fmt.Sprintf("trak%d", trakCnt)
+				trakCnt++
+			}
+			// fmt.Println(v.Type)
+		}
+		fmt.Println(acnt)
+
+	}
+
+}
